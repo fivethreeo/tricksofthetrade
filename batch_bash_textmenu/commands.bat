8f9f1e626f7ba2181dc28848efae136788c66c9b
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2015-06-11 02:14:09 +0200

    update scripts

--- batch_bash_textmenu/commands.bat
+++ batch_bash_textmenu/commands.bat
@@ -9,7 +9,9 @@ echo 3: gulp build
 echo 4: gulp serve
 echo 5: npm install
 echo 6: bower install
-echo 7: git checkout-index -a --prefix=..\dir\ (export)
+echo 7: virtualenv
+echo 8: pip install
+echo 9: git checkout-index -a --prefix=..\dir\ (export)
 echo.
 
 set /p command="Choose command number:"
@@ -35,6 +37,34 @@ if /I "%command%" EQU "6" (
   bower install !args!
 )
 if /I "%command%" EQU "7" (
+  set /p envname="Environment name (default: env):"
+  if /I "!envname!" EQU "" (
+    set envname=env
+  )
+  set /p sitepackages="Use site packages (y/n default: y):"
+  if /I "!sitepackages!" NEQ "n" (
+    set sitepackagesargs=--system-site-packages
+  )
+  set /p args="Arguments:"
+  virtualenv !envname! !sitepackagesargs! !args!
+)
+if /I "%command%" EQU "8" (
+  set /p envname="Environment name (default: env):"
+  if /I "!envname!" EQU "" (
+    set envname=env
+  )
+  set /p userequirements="Use requirements (y/n default: y):"
+  if /I "!userequirements!" NEQ "n" (
+    set /p requirements="Requirements file (default: requirements.txt):"
+    if /I "!requirements!" EQU "" (
+      set requirements=requirements.txt
+    )
+    set requirementssargs=-r !requirements!
+  )
+  set /p args="Arguments:"
+  !envname!\Scripts\pip.exe install !requirementssargs! !args!
+)
+if /I "%command%" EQU "9" (
 set /p directory="Export to directory (end with slash):"
 if not exist "!directory!\." (
   mkdir !directory!
--- batch_bash_textmenu/commands.sh
+++ batch_bash_textmenu/commands.sh
@@ -8,7 +8,9 @@ echo "3: gulp build"
 echo "4: gulp serve"
 echo "5: npm install"
 echo "6: bower install"
-echo "7: git checkout-index -a --prefix=../dir/ (export)"
+echo "7: virtualenv"
+echo "8: pip install"
+echo "9: git checkout-index -a --prefix=../dir/ (export)"
 echo
 
 echo -n "Choose command number:"
@@ -39,6 +41,41 @@ case "$command" in
     bower install $commandarguments
     ;;
   "7")
+    echo -n "Environment name (default: env):"
+    read envname
+    if [ -z "$envname" ]; then
+      envname="env"
+    fi
+    echo -n "Use site packages (y/n default: y):"
+    read sitepackages
+    if [ "$sitepackages" != "n" ]; then
+      sitepackagesargs=--system-site-packages
+    fi
+    echo -n "Arguments:"
+    read commandarguments
+    echo virtualenv $envname $sitepackagesargs $commandarguments
+    ;;
+  "8")
+    echo -n "Environment name (default: env):"
+    read envname
+    if [ -z "$envname" ]; then
+      envname="env"
+    fi
+    echo -n "Use requirements (y/n default: y):"
+    read userequirements
+    if [ "$userequirements" != "n" ]; then
+      echo -n "Requirements file (default: requirements.txt):"
+      read requirements
+      if [ -z "$requirements" ]; then
+        requirements="requirements.txt"
+      fi
+      requirementssargs="-r $requirements"
+    fi
+    echo -n "Arguments:"
+    read commandarguments
+    echo $envname/bin/pip install $requirementssargs $commandarguments
+    ;;
+  "9")
     echo -n "Export to directory (end with backslash):"
     read directory
     if [ ! -d "$directory" ]; then
